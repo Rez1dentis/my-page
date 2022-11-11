99c140f3cebfb477a14c3d5376f5626faf77425e
Ivan Kosenko <jean.kosenko@gmail.com>
2022-11-12 00:29:25 +0300

    start

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'react/prop-types': 0,
+    'react/no-array-index-key': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/jsx-no-constructed-context-values': 0,
+  },
+};
